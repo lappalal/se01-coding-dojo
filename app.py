f0e358a6c74622132b7dc031342d179e614385ac
Jakob Sautner <jakob@silverjanuary.speedport.ip>
2021-12-01 16:16:06 +0100

    stuck on booleans

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 board = [['.','.','*'], ['.','.','.'], ['.','*','.']]
 
 # Get adjacent cells
+# position is a list [0,0]
 def adjacentCells(position):
     adj = []
     
@@ -11,22 +12,27 @@ def adjacentCells(position):
             (newX, newY) = (position[0]+row, position[1]+column)  # adjacent cell
             
             if (newX in range(0, len(board[0]))) and (newY in range(0,len(board))) and (row, column) != (0, 0):
-                adj.append((newX, newY))
+                adj.append([newX, newY])
     
     return adj
 
+print(adjacentCells([0,0]))
+
 # Check if any given spot is a mine
-def isMine(spot):
-    if spot == '*':
+# input as [0,0]
+def isMine(x):
+    print(board[x[0]][x[1]])
+    if board[x[0]][x[1]] == '*':
         True
     else:
         False
+
+print(board[0][2])
+print(isMine([0,2]))
 
 # Iterate over every cell to check adjacent cells for mines
 for column in range(len(board)):
     for row in range(len(board[column])):
         cell = [row,column]
-        #print(adjacentCells(cell))
         for mine in range(len(adjacentCells(cell))):
-            if isMine(adjacentCells(cell)[mine]) == True:
-                print(cell)
+            continue
